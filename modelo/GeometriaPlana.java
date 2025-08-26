597167f048d1880fc7a66c238656140f5601bd80
Isa Kramm <134699029+isadorakramm@users.noreply.github.com>
2025-08-26 09:14:07 -0300

    Geometria Plana

--- modelo/GeometriaPlana.java
+++ modelo/GeometriaPlana.java
@@ -1,44 +1,43 @@
 package modelo;
 
-import java.lang.Math;
-
 public class GeometriaPlana {
 
-    private double lado, base, altura, diagonalMaior, diagonalMenor, baseMaior, baseMenor, raio;
+    private double ladoA, ladoB, base, altura, diagonalMaior, diagonalMenor, baseMaior, baseMenor, raio;
 
-    // construtor
-
-    public quadrado (double lado) {
-        this.base = lado
+    public void setQuadrado (double ladoA) {
+        this.ladoA = ladoA;
     }
 
-    public retangulo (double base, double altura) {
+    public void setRetangulo (double base, double altura) {
         this.base = base;
         this.altura = altura;
     }
 
-    public paralelogramo (double base, double altura) {
+    public void setParalelogramo (double base, double altura, double ladoA, double ladoB) {
         this.base = base;
         this.altura = altura;
+        this.ladoA = ladoA;
+        this.ladoB = ladoB; 
     }
 
-    public losango (double diagonalMaior, double diagonalMenor) {
+    public void setLosango (double diagonalMaior, double diagonalMenor, double ladoA) {
         this.diagonalMaior = diagonalMaior;
         this.diagonalMenor = diagonalMenor;
+        this.ladoA = ladoA;
     }
 
-    public trapezio (double baseMaior, double baseMenor, double altura) {
+    public void setTrapezio (double baseMaior, double baseMenor, double altura) {
         this.baseMaior = baseMaior;
         this.baseMenor = baseMenor;
         this.altura = altura;
     }
 
-    public triangulo (double base, double altura) {
+    public void setTriangulo (double base, double altura) {
         this.base = base;
         this.altura = altura;
     }
 
-    public circulo (double raio) {
+    public void setCirculo (double raio) {
         this.raio = raio;
     }
 
@@ -46,32 +45,32 @@ public class GeometriaPlana {
     // áreas
 
     // área do quadrado
-    public areaQuadrado (double lado) {
-        return Math.pow(lado, 2);
+    public double areaQuadrado () {
+        return Math.pow(ladoA, 2);
     }
 
     // área do retângulo e do paralelogramo
-    public areaRP (double base, double altura) { 
+    public double areaRP () { 
         return base * altura;
     }
 
     // área do losango
-    public areaLosango (double diagonalMaior, double diagonalMenor) {
+    public double areaLosango () {
         return (diagonalMaior * diagonalMenor) / 2;
     }
 
     // área do trapézio
-    public areaTrapezio (double baseMaior, double baseMenor, double altura) {
+    public double areaTrapezio () {
         return ((baseMaior + baseMenor) * altura) / 2;
     }
 
     // triângulos
-    public areaTriangulo (double base, double altura) {
+    public double areaTriangulo () {
         return (base * altura) / 2;
     }
 
     // área do círculo
-    public areaCirculo (double raio) {
+    public double areaCirculo () {
         return Math.PI * Math.pow(raio, 2);
     }
 
@@ -79,19 +78,32 @@ public class GeometriaPlana {
     // perímetros
 
     // perímetro do quadrado
-    public perimetroQuadrado (double lado) {
-        return 4 * lado;
+    public double perimetroQuadrado () {
+        return 4 * ladoA;
     }
 
     // perímetro do retângulo e paralelogramo
-    public perimetroRP (double base, double altura) {
+    public double perimetroRP () {
         return 2 * (base + altura);
     }
 
     // perímetro do losango
-    public perimetroLosango (double lado) {
-        return 4 * lado;
+    public double perimetroLosango () {
+        return 4 * ladoA;
     }
 
-    //
+    // perímetro do trapézio
+    public double perimetroTrapezio () {
+        return baseMaior + baseMenor + ladoA + ladoB;
+    }
+
+    // perímetro do triângulo
+    public double perimetroTriangulo () {
+        return base + ladoA + ladoB;
+    }
+
+    // perímetro do círculo
+    public double perimetroCirculo () {
+        return 2 * Math.PI * raio;
+    }
 }
