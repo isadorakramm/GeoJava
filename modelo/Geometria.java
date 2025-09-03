eeff7e93b72677545ded1a96e0e99ddc2936b7c6
Isa Kramm <134699029+isadorakramm@users.noreply.github.com>
2025-09-03 08:03:36 -0300

    Update Geometria.java

--- modelo/Geometria.java
+++ modelo/Geometria.java
@@ -3,16 +3,10 @@ package modelo;
 public class Geometria {
     public static void main(String[] args) {
 
-        GeometriaEspacial minhaesfera = new GeometriaEspacial();
+        // metodos da geometria plana:
 
-        minhaesfera.setEsfera(3); 
-        double volumedaEsfera = minhaesfera.volumeEsfera();
-        System.out.println("Volume da esfera: " + volumedaEsfera);
-
-        GeometriaEspacial meuprisma = new GeometriaEspacial();
-        meuprisma.setPrisma(9, 12, 5);
-        double volumedoPrisma = meuprisma.volumePrisma();
-        System.out.println("Volume do prisma: " + volumedoPrisma);
+        // quadrado
+        }
     }
 }
 
